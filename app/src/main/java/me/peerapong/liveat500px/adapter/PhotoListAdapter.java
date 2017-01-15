debd9fce40954afd5cd73a1cff0b8c859817215b
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2017-01-15 14:00:48 +0700

    Lecture 135 Completed

---
+++ app/src/main/java/me/peerapong/liveat500px/adapter/PhotoListAdapter.java
@@ -0,0 +1,33 @@
+package me.peerapong.liveat500px.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import me.peerapong.liveat500px.view.PhotoListItem;
+
+/**
+ * Created by peerapong on 1/15/17.
+ */
+
+public class PhotoListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 10000;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return new PhotoListItem(parent.getContext());
+    }
+}
