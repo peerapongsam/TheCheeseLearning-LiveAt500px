665187da828c535bb8f2c492700ff8c5328c46a1
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2017-01-15 15:44:30 +0700

    Lecture 145 Completed

--- app/src/main/java/me/peerapong/liveat500px/manager/http/HttpManager.java
+++ app/src/main/java/me/peerapong/liveat500px/manager/http/HttpManager.java
@@ -2,6 +2,8 @@ package me.peerapong.liveat500px.manager.http;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
 
 import retrofit2.Retrofit;
@@ -19,9 +21,14 @@ public class HttpManager {
     private HttpManager() {
         mContext = Contextor.getInstance().getContext();
 
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://nuuneoi.com/courses/500px/")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
         service = retrofit.create(ApiService.class);
