cef0838c45e1be698370f7ea556079bee769f616
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2017-01-16 14:12:55 +0700

    Lecture 177 Completed

--- app/src/main/java/me/peerapong/liveat500px/activity/MoreInfoActivity.java
+++ app/src/main/java/me/peerapong/liveat500px/activity/MoreInfoActivity.java
@@ -2,6 +2,7 @@ package me.peerapong.liveat500px.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import me.peerapong.liveat500px.R;
 import me.peerapong.liveat500px.fragment.MoreInfoFragment;
@@ -22,6 +23,16 @@ public class MoreInfoActivity extends AppCompatActivity {
     }
 
     private void initInstances() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
